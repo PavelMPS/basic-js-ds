029a6839cb94c52fbed8d2ddcb4d793107ee572a
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-10-13 16:09:19 +0300

    feat: add solution BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,130 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 module.exports = class BinarySearchTree {
+  constructor(){
+    this.rootNode=null;
+  }
+
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let dataNode= new Node(data);
+    if(this.rootNode===null){
+      this.rootNode=dataNode;
+    }else{
+      this.addNode(this.rootNode,dataNode)
+    }
+  }
+  addNode(node, dataNode){
+    if(dataNode.data<node.data){
+      if(node.left===null){
+         node.left=dataNode;
+      }else{
+        this.addNode(node.left, dataNode);
+      }
+    }else if(node.right===null){
+      node.right=dataNode;
+    }else{
+      this.addNode(node.right, dataNode);
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data){
+    if(this.rootNode===null){
+      return false;
+    } else return this.hasNode(this.rootNode, data);   
+  }
+  hasNode(node, data){
+    if(node===null){
+      return false;
+    }
+    if(data<node.data){
+      return this.hasNode(node.left, data);
+    }else if(data>node.data){
+      return this.hasNode(node.right, data);
+    } else return true;
+  }
+    
+
+  find(data) {
+    if(this.rootNode===null){
+      return null;
+    }else return this.findNode(this.rootNode, data);
+  }
+  findNode(node, data){
+    if(node===null){
+      return null;
+    }else if(data<node.data){
+      return this.findNode(node.left, data);
+    }else if(data>node.data){
+      return this.findNode(node.right, data);
+    }else return node;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootNode = this.removeNode(this.rootNode, data);
   }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeNode(node, data){
+    if(node===null){
+      return false;
+    }else if(node.data>data){
+      node.left=this.removeNode(node.left, data);
+    }else if(node.data<data){
+      node.right=this.removeNode(node.right, data);
+    }else if(node.left===null&&node.right===null){
+            node=null;
+          }
+          if(node.left===null){
+            node=node.right;
+          }
+          if(node.right===null){
+            node=node.left;
+          }
+          let dataNode = this.minNode(node.right);
+          node.data=dataNode.data;
+          node.right=this.removeNode(node.right, dataNode.data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.rootNode===null){
+      return null;
+    }else if(this.rootNode.left===null){
+      return this.rootNode;
+    }else return this.minNode(this.rootNode);
+  }
+  minNode(node){
+    if(node.left===null){
+      return node.data;
+    }else return this.minNode(node.left);
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.rootNode===null){
+      return null;
+    }else if(this.rootNode.right===null){
+      return this.rootNode;
+    }else return this.maxNode(this.rootNode);
+  }
+  maxNode(node){
+    if(node.right===null){
+      return node.data;
+    }else return this.maxNode(node.right);
   }
 
 }
+
+
+class Node{
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;		
+}
+}
+
+ 
