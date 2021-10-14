774ae557cf185b64207cd6f9394c0953b8770577
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-10-14 19:02:50 +0300

    feat: update queque

--- src/st-queue.js
+++ src/st-queue.js
@@ -13,40 +13,37 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+ 
 module.exports = class Queue {
-constructor(){
-  this.head=null;
-  this.tail=null;
-  this.length=0;
-}
-  
+  constructor(){
+    this.head=null;
+    this.tail=null;
+    this.length=0;
+  }
+ 
 
-  getUnderlyingList() {
+  getUnderlyingList() {//Получить связанный список
     return this.head;
      }
   
 
-  enqueue(value) {
-   let newNode = new ListNode(value);
-   if(this.length===0){
-     this.head=newNode;
-     this.tail=newNode;
-   }else{
-     this.tail=newNode;
-     this.tail.next=newNode;
-   }
-   this.length++;
-   return this;
+  enqueue(value) {//Добавить элемент в очередь
+    const newListNode=new ListNode(value);
+    if(this.head===null){
+      this.head=newListNode;
+      this.tail=newListNode;
+    }else{
+      this.tail.next=newListNode;
+      this.tail=newListNode;
+    }
+    this.length++;
   }
 
-  dequeue() {
-    let elem=this.head;
-    if(elem.next!=null){
-      this.head=null;
-      this.length--
-    }
-
-    }
-  
-
+  dequeue() {//Убрать первый элемент из очереди
+    let tempNodeList=this.head.value;
+    this.head=this.head.next;
+    this.length--
+    return tempNodeList;
 }
+}
+
